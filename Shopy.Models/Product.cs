174ff088adc48f18be1b0dfa5d593b626ec44240
AbanoubSaweris <abanoubphelopos12@gmail.com>
2024-07-15 22:03:09 +0200

    update Home Page design

--- Shopy.Models/Product.cs
+++ Shopy.Models/Product.cs
@@ -24,20 +24,20 @@ namespace Shopy.Models
         [Required]
         public string Auther { get; set; }
         [Required]
-        [Range(1,1000)]
+        [Range(1,10000)]
         [Display(Name= "List Price")]
         public double ListPrice { get; set; }
 
         [Required]
-        [Range(1, 1000)]
+        [Range(1, 10000)]
         [Display(Name = "Price when buy 1-50")]
         public double Price { get; set; }
         [Required]
-        [Range(1, 1000)]
+        [Range(1, 10000)]
         [Display(Name = "Price when buy +50")]
         public double Price50 { get; set; }
         [Required]
-        [Range(1, 1000)]
+        [Range(1, 10000)]
         [Display(Name = "Price when buy +100")]
         public double Price100 { get; set; }
         [ForeignKey("Category")]
